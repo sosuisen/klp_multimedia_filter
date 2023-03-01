0b84670869955136c28248218c3f6c39af424568
sosuisen <hidekazu.kubota@gmail.com>
2023-03-01 19:32:26 +0900

    fix: use vec2

--- mosaic.js
+++ mosaic.js
@@ -1,31 +1,5 @@
 PIXI.filters.MosaicFilter = class extends PIXI.Filter {
   constructor() {
-    const vertexSrc = `
-        uniform vec4 outputFrame;
-        attribute vec2 aVertexPosition;
-        uniform mat3 projectionMatrix;
-        uniform vec4 inputSize;
-        varying vec2 vTextureCoord;
-        varying vec2 vBlurTexCoords[9];
-
-        vec4 filterVertexPosition( void )
-        {
-            vec2 position = aVertexPosition * outputFrame.zw + outputFrame.xy;
-            return vec4((projectionMatrix * vec3(position, 1.0)).xy, 0.0, 1.0);
-        }
-        
-        vec2 filterTextureCoord( void )
-        {
-            return aVertexPosition * outputFrame.zw * inputSize.zw;
-        }
-        
-        void main(void)
-        {
-            gl_Position = filterVertexPosition();
-            vTextureCoord = filterTextureCoord();
-        }
-    `;
-
     const fragmentSrc = `
         precision highp float; // outputFrame を使う場合、highp 指定が必要
         uniform vec4 outputFrame;
@@ -39,25 +13,17 @@ PIXI.filters.MosaicFilter = class extends PIXI.Filter {
 
           float mosaicSize = 5.0;
           // スクリーン座標系へ変換
-          float screenX = vTC.x * outputFrame.z;
+          vec2 screenPosition = vTC.xy * outputFrame.zw;
           // 1辺がmosaicSizeの正方形の左上隅の座標
-          float cornerX = floor(screenX / mosaicSize) * mosaicSize;
+          vec2 cornerPosition = floor(screenPosition / mosaicSize) * mosaicSize;
           // 正規化座標へ戻して代入
-          vTC.x = cornerX / outputFrame.z;
-
-          // スクリーン座標系へ変換
-          float screenY = vTC.y * outputFrame.w;
-          // 1辺がmosaicSizeの正方形の左上隅の座標
-          float cornerY = floor(screenY / mosaicSize) * mosaicSize;
-          // 正規化座標へ戻して代入
-          vTC.y = cornerY / outputFrame.w;
-
+          vTC = cornerPosition / outputFrame.zw;
           gl_FragColor = texture2D(uSampler, vTC);
         }
       `;
 
     super(
-      vertexSrc,
+      null,
       fragmentSrc,
       {}
     );

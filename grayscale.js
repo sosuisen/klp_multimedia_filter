27b63d61671c35046c70aa59e1495e79fd902e8e
sosuisen <hidekazu.kubota@gmail.com>
2023-06-22 10:09:05 +0900

    fix: add answer of kadai-kihon

--- grayscale.js
+++ grayscale.js
@@ -9,7 +9,13 @@ export const GrayscaleFilter = class extends PIXI.Filter {
           vec4 color = texture2D(uSampler, vTextureCoord);
 
           // color をグレースケールへ変換してください。
-
+          float gray = color.r * 0.2126 + color.g * 0.7152 + color.b * 0.0722;
+          color.r = color.g = color.b = gray;
+          if (gray >= 0.5){
+            gray =1.0;
+          }else {
+            gray =0.0;
+          }
           gl_FragColor = color;
         }
       `;

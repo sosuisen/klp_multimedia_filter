e0eb2514915e5bb2aa154599c5749c59ceca2e3e
sosuisen <hidekazu.kubota@gmail.com>
2025-04-01 14:44:06 +0900

    fix: use 300 es

--- grayscale.frag
+++ grayscale.frag
@@ -1,12 +1,12 @@
 #version 300 es
 precision mediump float;
 
-uniform sampler2D uSampler;
+uniform sampler2D uTexture;
 in vec2 vTextureCoord;
 out vec4 fragColor;
 
 void main(void) {
-    vec4 color = texture(uSampler, vTextureCoord);
+    vec4 color = texture(uTexture, vTextureCoord);
 
     // colorの値を明るさ情報のみ（グレースケール）へ変換
     float gray = color.r * 0.2126f + color.g * 0.7152f + color.b * 0.0722f;
--- mosaic.js
+++ mosaic.js
@@ -1,23 +1,14 @@
 import * as PIXI from 'pixi.js';
+import fragment from './mosaic.frag';
+import vertex from './default.vert';
+
 export const MosaicFilter = class extends PIXI.Filter {
   constructor() {
-    const fragmentSrc = `
-      precision highp float;
-      uniform vec4 inputSize;
-      varying vec2 vTextureCoord;
-      uniform sampler2D uSampler;
-
-      void main(void) {
-
-
-
-      }
-    `;
-
-    super(
-      null,
-      fragmentSrc,
-      {}
-    );
+    super({
+      glProgram: new PIXI.GlProgram({
+        fragment,
+        vertex,
+      })
+    });
   }
 };
